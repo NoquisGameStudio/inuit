da7fd9d2cfceb669a1c6ad6fd6f8325cc3e2f301
ToniLaraBuenrostro <tonilarabuenrostro@gmail.com>
2023-02-12 15:27:25 +0100

    Cambios juego doodleJump Toni

--- pruebas 2d/Assets/Toni/doodlejump/scripts/DoodleJumpMovement.cs
+++ pruebas 2d/Assets/Toni/doodlejump/scripts/DoodleJumpMovement.cs
@@ -9,23 +9,48 @@ public class DoodleJumpMovement : MonoBehaviour
     public float speed = 5.0f;
     Vector2 mov;
     Animator anim;
+    private Rigidbody2D rb;
+    private bool jump = false;
+
+    private PlayerInput input;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        input = GetComponent<PlayerInput>();
+
+        input.actions["Player/Move"].performed += OnMove;
+        input.actions["Player/Move"].canceled += OnMove;
+
+        input.actions["Player/Jump"].performed += OnJump;
+
     }
 
-    public void OnMove(InputValue input)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        mov = input.Get<Vector2>();
+        mov = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jump = true;
     }
 
     private void FixedUpdate()
     {
-        anim.SetFloat("x", mov.x);
-        anim.SetFloat("y", mov.y);
 
-        transform.Translate(Vector2.up * speed * mov.y * Time.fixedDeltaTime);
-        transform.Translate(Vector2.right * speed * mov.x * Time.fixedDeltaTime);
+        if(mov != null)
+        {
+            anim.SetFloat("x", mov.x);
+            transform.Translate(Vector2.right * speed * mov.x * Time.fixedDeltaTime);
+        }
+
+        if (jump)
+        {
+            rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+            jump = false;
+        }
     }
 }
