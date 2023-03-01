821e799a331fc205a0d01f6fa906b35572de0ef7
ToniLaraBuenrostro <tonilarabuenrostro@gmail.com>
2023-03-01 20:36:20 +0100

    Comienzo juego rompecabezas helado

---
+++ Proyecto_Inuit/Assets/Toni/RompecabezasHelado/Scripts/MovementRompecabezas.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementRompecabezas : MonoBehaviour
+{
+    private Animator anim;
+    private Vector2 move;
+    private RaycastHit2D hit;
+    private bool moving = false;
+    private Vector2 oldmove;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    private void Update()
+    {
+        if(move != null)
+        {
+            if(!moving)
+            {
+                hit = Physics2D.Raycast(transform.position, move);
+                oldmove = move;
+            }
+
+            if (hit.collider != null)
+            {
+                if (Mathf.Abs(hit.distance) > 0.25)
+                {
+                    moving = true;
+                    Vector2 dest = hit.transform.position - new Vector3(oldmove.x, oldmove.y);
+                    transform.position = Vector2.Lerp(transform.position, dest, Time.deltaTime * 1.2f);
+
+                    if (((int)transform.position.magnitude) == (int)(dest.magnitude))
+                    {
+                        Debug.Log("entro");
+                        moving = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public void OnMove(InputValue input)
+    {
+        move = input.Get<Vector2>();
+    }
+}
