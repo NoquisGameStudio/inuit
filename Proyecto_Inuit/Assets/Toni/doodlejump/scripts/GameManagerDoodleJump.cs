0e5d7f4ff03bf527231939c551d1a9bbf647785f
ToniLaraBuenrostro <tonilarabuenrostro@gmail.com>
2023-03-27 10:41:34 +0200

    Cambio UI doodlejump

--- Proyecto_Inuit/Assets/Toni/doodlejump/scripts/GameManagerDoodleJump.cs
+++ Proyecto_Inuit/Assets/Toni/doodlejump/scripts/GameManagerDoodleJump.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManagerDoodleJump : MonoBehaviour
 {
     public GameObject platformPrefab;
     public Camera cam;
